bacaeeb5b986faae90d0b7a49be26954e90d5c38
Kerem Mert <kerem37mert@yahoo.com>
2024-07-15 18:21:01 +0300

    Traversal func added

--- LinkedList.go
+++ LinkedList.go
@@ -1,11 +1,13 @@
 package linked_list
 
+import "fmt"
+
 type LinkedList struct {
 	head *Node[any]
 }
 
+// Add Node
 func (L *LinkedList) Add(value any) {
-
 	var newNode *Node[any] = new(Node[any])
 	newNode.data = value
 	newNode.next = nil
@@ -18,5 +20,20 @@ func (L *LinkedList) Add(value any) {
 			temp = temp.next
 		}
 		temp.next = newNode
+	}
+}
+
+// Linked List Traversal
+
+func (L *LinkedList) Traversal() {
+	if L.head == nil {
+		fmt.Println("Your linked list is empty")
+	} else {
+		var temp *Node[any] = L.head
+		for temp != nil {
+			fmt.Printf("[%v] -> ", temp.data)
+			temp = temp.next
+		}
+		fmt.Printf("nil")
 	}
 }
